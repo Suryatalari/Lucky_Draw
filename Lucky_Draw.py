c2fa5ffc97eb578aafb7204d4922a2e1777932ce
surya talari <tejuts2000@gmail.com>
2020-07-22 14:06:13 +1000

    Commiting with comments.

--- Lucky_Draw.py
+++ Lucky_Draw.py
@@ -1,6 +1,6 @@
 import random
-players=random.sample(range(300,325),10)
-for i in range(0,10):
-	print("Player",i+1,":",players[i])
-num=players.index(max(players))+1
+players=random.sample(range(300,325),10)                #Choosing 10 random numbers between 300 and 325.
+for i in range(0,10):                                   
+	print("Player",i+1,":",players[i])                  #Display the random numbers assigned to each player.
+num=players.index(max(players))+1                       #Determine the maximum draw among the players.
 print("The winner is Player",num,":",max(players))
